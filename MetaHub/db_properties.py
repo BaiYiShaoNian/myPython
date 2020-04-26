333ed4ea9030f7f9cecd4da058f2ae7be4810cbf
huangxuanfeng <huangxuanfeng@gotokeep.com>
2020-04-26 19:07:39 +0800

    加密

--- MetaHub/db_properties.py
+++ MetaHub/db_properties.py
@@ -12,58 +12,57 @@ sys.setdefaultencoding('utf-8')
 
 MYSQL_CONFIG_DEV = {
     'metahub': {
-        'host': '172.16.1.31',
+        'host': '******',
         'port': 3306,
-        'name': 'keep_melon_server',
-        'username': 'root',
-        'passwd': 'keep_1234'
+        'name': '******',
+        'username': '******',
+        'passwd': '******'
     }
 }
 
 MYSQL_CONFIG_PRE = {
     'metahub': {
-        'host': 'premysql01.rds.svc.ali.keep',
+        'host': '******',
         'port': 3306,
-        'name': 'metahub',
-        'username': 'k_metahub',
-        'passwd': 'r3fyfi4OL6TGUnVM'
+        'name': '******',
+        'username': '******',
+        'passwd': '******'
     },
     'metahub2': {
-        'host': 'premysql01.rds.svc.ali.keep',
+        'host': '******',
         'port': 3306,
-        'name': 'tagm',
-        'username': 'k_tagm',
-        'passwd': '3MqNobFeSO8Rsauu'
+        'name': '******',
+        'username': '******',
+        'passwd': '******'
     }
 }
 
 MYSQL_CONFIG_ONLINE = {
     'metahub': {
-        'host': 'metahub.rds.svc.ali.keep',
+        'host': '******',
         'port': 3306,
-        'name': 'metahub',
-        'username': 'k_metahub',
-        'passwd': 'r3fyfi4OL6TGUnVD'
+        'name': '******',
+        'username': '******',
+        'passwd': '******'
     }
 }
 
 HIVE_CONFIG_ONLINE = {
-    'host': 'bj-hd-di-14.ali.keep',
+    'host': '******',
     'port': 10000,
     'username': 'hdfs'
 }
 
 HIVE_META_CONFIG_DI = {
     'hive_new': {
-        'host': 'cdh.rds.svc.ali.keep',
+        'host': '******',
         'port': 3306,
-        'name': 'hive_new',
-        'username': 'datakeep',
-        'passwd': 'R6dHdnZ9n4qHclLy'
+        'name': '******',
+        'username': '******',
+        'passwd': '******'
     }
 }
 
-# mysql -hcdh.rds.svc.ali.keep -udatakeep -pR6dHdnZ9n4qHclLy hive_new
 # tbls，表名，TBL_ID，CREATE_TIME，TBL_NAME，TBL_TYPE，SD_ID
 # sds，文件存储基本信息，SD_ID，CD_ID，
 # partition_keys, 分区名
